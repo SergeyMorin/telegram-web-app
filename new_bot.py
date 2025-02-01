9d66e51c0fc5b0ad51c8a72f061f8be13ed37ab2
SergeyMorin <serega-morin@yandex.ru>
2025-02-01 11:50:20 +0300

    web page added

--- new_bot.py
+++ new_bot.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
 import telebot
+from telebot import types
+
 
 load_dotenv()
 
@@ -13,9 +15,16 @@ if not api_token:
 
 bot = telebot.TeleBot(api_token)
 
+
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Hello! Welcome to the new project.")
+    markup = types.InlineKeyboardMarkup()
+    web_app_button = types.InlineKeyboardButton("Открыть Web App",
+                                                url="https://sergeymorin.github.io/telegram-web-app/")
+    markup.add(web_app_button)
+
+    bot.send_message(message.chat.id, "Привет! Нажми на кнопку, чтобы открыть Web App.", reply_markup=markup)
+
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
